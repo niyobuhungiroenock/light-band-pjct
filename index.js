5f1a343ddbac893fcbe98a08ea32174401d9ca60
niyobuhungiro enock <133653892+niyobuhungiroenock@users.noreply.github.com>
2023-09-16 09:38:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+/*const arrows=document.querySelectorAll(".arrow");
+const movielists=document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i) => {
+    arrow.addEventListener("click",()=>{
+        movielists[i].style.transform=`translateX(${
+            movielists[i].computedStylemap().get("transform")(0).x.value-300}px)`;
+    });
+});*/
